bb912843532495f6d234771d573013babf4d517e
Binh Tran <thanhbinh.tran93@gmail.com>
2018-03-05 13:58:33 +0200

    Test the DELETE /dotos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,40 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 })
+
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				var todo = Todo.findById(hexId).then((todo) => {
+					expect(todo).toNotExist();
+					done();
+				}).catch(e => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		request(app)
+			.delete(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if ObjectID is invalid', (done) => {
+		request(app)
+			.delete(`/todos/435345`)
+			.expect(404)
+			.end(done);
+	});
+});
